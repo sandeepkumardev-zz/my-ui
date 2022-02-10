9c34c0e65a5b691364c756fc6a6666d3301be09d
Sandeep <sandeepypb@gmail.com>
2022-02-10 21:20:49 +0530

    feat: 🎸 added links

--- docs/getting-started/index.js
+++
@@ -1,9 +0,0 @@
-const toggleDrawerModel = document.getElementById("toggleRuiDrawerModel");
-
-function closeDrawerModel() {
-    toggleDrawerModel.style.display = "none";
-}
-
-function showDrawerModel() {
-    toggleDrawerModel.style.display = "block";
-}
