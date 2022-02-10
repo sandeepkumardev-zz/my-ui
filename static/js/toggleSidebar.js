d966f0126f06f5df2c830d4d59c43d026da69abb
Sandeep <sandeepypb@gmail.com>
2022-02-10 21:01:20 +0530

    feat: 🎸 added js in a single file

---
+++ static/js/toggleSidebar.js
@@ -0,0 +1,9 @@
+const toggleDrawerModel = document.getElementById("toggleRuiDrawerModel");
+
+function closeDrawerModel() {
+    toggleDrawerModel.style.display = "none";
+}
+
+function showDrawerModel() {
+    toggleDrawerModel.style.display = "block";
+}
